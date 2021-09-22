33cb1b8064cf9a5cb732ade23733c2029c138189
aorich <rich.aidan@gmail.com>
2021-09-22 13:31:16 -0400

    more console log stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
 
+var search = "sonic"
 
-fetch("https://openlibrary.org/subjects/" + typeArea.value + ".json?published_in=2000-2021")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
+fetch("https://openlibrary.org/subjects/" + search + ".json?published_in=2000-2021")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+
+        if (data.work_count == 0) {
+            console.log("No data");
+        }
+        else
             console.log(data);
-        })
+        console.log(data.works[0].title);
+        console.log(data.works[0].authors[0].name);
+
+
+    })
