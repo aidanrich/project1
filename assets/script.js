5076775c020246efa2dd023005804f37409941e3
Aaron Donelson <aaron.donelson23@gmail.com>
2021-09-22 14:29:35 -0400

    pulled image data, genres, and ratings

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,8 @@ var requestUrlgames = "https://api.rawg.io/api/games?key=61eab2930fd5479c99f315c
 var last3Months = "https://api.rawg.io/api/games?dates=2021-06-01,2021-09-01&platforms=187,4,186&key=61eab2930fd5479c99f315c0016527b5"
 var apiKey= "key=61eab2930fd5479c99f315c0016527b5";
 
+var displayInfo = document.getElementById('displayInfo');
+
 fetch(last3Months)
   //fetch is first calling for the URL the promising to wait until it is ready to
 
@@ -12,31 +14,42 @@ fetch(last3Months)
   })
   .then(function (data) {
 
-    for (let index = 0; index < data.length; index++) {
+
+      
+    
+    console.log(data);
+    for (let index = 0; index < data.results.length; index++) {
         
-        var backgroundImageEl = $('<img>');
-        backgroundImageEl.setAttribute("src", iconurl);
-        var genresEl = $('<p>');
-        var ratingsEl = $('<p>');
-        var divStyleEl = $('<div>')
-
-
-
-        var ratinngs 
-        var generes
-        var backgroundImage
         
-        backgroundImageEl.innerHTML= data.results[index].background_image;
-        genresEl.innerHTML = data.results[index].genres;
-        ratingsEl.innerHTML = data.results[index].ratings_count;
-
+    
+          
+        
+        var gameTitle = document.createElement('p'); 
+        var backgroundImageEl = document.createElement('img');       
+        var genresEl = document.createElement('p');
+        var ratingsEl = document.createElement('p');
+        var divStyleEl = document.createElement('div'); 
+        backgroundImageEl.setAttribute("src", imageBack);
+        
+        
+        gameTitle.innerHTML = data.results[index].name;
+        var imageBack =  data.results[index].short_screenshots[0].image;
+          genresEl.innerHTML = data.results[index].genres[0].name;
+          ratingsEl.innerHTML = data.results[index].ratings_count;
+          
+          var ratinngs 
+          var generes
+          var backgroundImage   
+          
         divStyleEl.append(backgroundImageEl);
         divStyleEl.append(genresEl);
         divStyleEl.append(ratingsEl);
-        
-        
-    }  
 
-    console.log(data);
+        displayInfo.append(divStyleEl);
+        
+    } 
+   
+
+    
 
 });
