4f5b4afe30b9e4745d0f8e16f0bbb62777ba8591
aorich <rich.aidan@gmail.com>
2021-09-22 13:40:28 -0400

    covers will be available now

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@ fetch("https://openlibrary.org/subjects/" + search + ".json?published_in=2000-20
             console.log(data);
         console.log(data.works[0].title);
         console.log(data.works[0].authors[0].name);
-
+        var bookCover = document.createElement('img');
+        bookCover.setAttribute("src", `https://ia600602.us.archive.org/view_archive.php?archive=/10/items/olcovers573/olcovers573-L.zip&file=${data.works[0].cover_id}-L.jpg`);
 
     })
