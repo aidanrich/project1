2da7462498ffbf2609bbd9f182dec825cce2928f
Aaron Donelson <aaron.donelson23@gmail.com>
2021-09-23 18:03:38 -0400

    nested fetch complete for drop down search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var requestUrlgames = "https://api.rawg.io/api/games?key=61eab2930fd5479c99f315c0016527b5";
 
 var displayInfo = document.getElementById('displayInfo');
+var gameInfo = document.getElementById('gameInfo');
 var saveButton = document.getElementById("Submit");
  $(function(){
             $("#gameInfo").hide();
@@ -138,8 +139,43 @@ fetch(requestUrlgames)
     
         
             fetch(requestUrlgames)
-            //fetch is first calling for the URL the promising to wait until it is ready to
+            
+            .then(function (response) {
+                return response.json();
         
+            })
+            .then(function (data) { 
+
+            var gameTitle = document.createElement('p');
+            var backgroundImageEl = document.createElement('img');
+            var genresEl = document.createElement('p');
+            var ownedEl = document.createElement('p');
+            var dateReleased = document.createElement('p');
+            var peoplePlaying = document.createElement('p');
+
+            var divStyleEl = document.createElement('div');
+            backgroundImageEl.setAttribute("src", imageBack);
+
+
+
+
+            var imageBack = data.results[index].short_screenshots[1].image;
+            gameTitle.innerHTML = "Game Title: " + " " + data.results[index].name;
+            dateReleased.innerHTML = "Release Date: " + " " + data.results[index].released;
+            genresEl.innerHTML = "Genre type:" + " " + data.results[index].genres[0].name;
+            ownedEl.innerHTML = "People who own this game:" + " " + data.results[index].added_by_status.owned;
+            peoplePlaying.innerHTML = "Number of people Playing:" + " " + data.results[index].added_by_status.playing;
+
+            divStyleEl.append(backgroundImageEl);
+            divStyleEl.append(gameTitle);
+            divStyleEl.append(dateReleased);
+            divStyleEl.append(genresEl);
+            divStyleEl.append(ownedEl);
+            divStyleEl.append(peoplePlaying);
+
+            gameInfo.append(divStyleEl);
+
+            });
         }
         
     });
